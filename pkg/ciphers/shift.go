cb98d9431917106493c50ee5f10253bd394a7257
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2023-06-30 15:14:20 +0200

    feat: implement `ShiftRune` without looping

--- pkg/ciphers/shift.go
+++ pkg/ciphers/shift.go
@@ -1,7 +1,26 @@
 package ciphers
 
-import "errors"
+import (
+	"fmt"
+)
+
+const LowerBoundLC = 'a'
+const UpperBoundLC = 'z'
+const LowerBoundUC = 'A'
+const UpperBoundUC = 'Z'
+
+const AlphabetLength = int(UpperBoundLC - LowerBoundUC)
 
 func ShiftRune(input rune, shift int) (*rune, error) {
-	return nil, errors.New("implement")
+	shift %= AlphabetLength
+
+	isASCII := input >= LowerBoundLC && input <= UpperBoundLC || input >= LowerBoundUC && input <= UpperBoundUC
+
+	if !isASCII {
+		return nil, fmt.Errorf("not an ascii character: '%c'", input)
+	}
+
+	result := input + rune(shift)
+
+	return &result, nil
 }
